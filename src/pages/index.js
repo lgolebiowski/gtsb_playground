9a285e57c7a2092dbdd6518f6a599d521ca41181
Lukasz Golebiowski <lukasz.golebiowski@gmail.com>
2020-01-08 11:51:31 +1100

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,16 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>In the current consumer computers, every program runs for a specific time slot, and then it stops its execution to let another program continue its execution. This thing runs in a cycle so fast that’s impossible to notice, and we think our computers run many programs simultaneously, but this is an illusion (except on multiprocessor machines).
+    Programs internally use interrupts, a signal that’s emitted to the processor to gain the attention of the system.
+    I won’t go into the internals of this, but just keep in mind that it’s normal for programs to be asynchronous, and halt their execution until they need attention, and the computer can execute other things in the meantime. When a program is waiting for a response from the network, it cannot halt the processor until the request finishes.
+    Normally, programming languages are synchronous, and some provide a way to manage asynchronicity, in the language or through libraries. C, Java, C#, PHP, Go, Ruby, Swift, Python, they are all synchronous by default. Some of them handle async by using threads, spawning a new process.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <br/>
+    <Link to="/page-3/">Go to page 3</Link>
   </Layout>
 )
 
